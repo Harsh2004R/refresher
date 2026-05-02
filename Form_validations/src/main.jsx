d7e3d7255167e88b1ac0865a7dcdd0ba6d5f52d0
Harsh2004R <harshsharmaktm03@gmail.com>
2026-05-02 21:05:15 +0530

    Form Validation using React-Hook-Form

---
+++ Form_validations/src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider as ChakraProvider } from "./components/ui/provider"
+
+createRoot(document.getElementById("root")).render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+);
