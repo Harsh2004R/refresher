6026fb450f08de974de59d84d6a73fa23e73c542
Harsh2004R <harshsharmaktm03@gmail.com>
2026-05-03 14:17:19 +0530

    Add Login Validations

--- Form_validations/src/App.jsx
+++ Form_validations/src/App.jsx
@@ -1,104 +1,11 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import { Box, Center, Text, Input, Field, Button } from "@chakra-ui/react";
+import Login from "./Forms/Login";
+
 function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-
-  const handleForm = (data) => {
-    console.log(data);
-  };
-
   return (
-    <Center flexDirection={"column"} w="100%" minH="100vh">
-      <Text p="15px" color="wheat" textAlign={"center"} fontSize={"2xl"}>
-        Form Validation
-      </Text>
-      <Center
-        minW="50%"
-        minH="70vh"
-        p="5"
-        borderRadius={"lg"}
-        flexDirection={"column"}
-        border={"1px solid #888798"}
-      >
-        <form onSubmit={handleSubmit(handleForm)}>
-          <Field.Root required>
-            <Field.Label>First Name</Field.Label>
-            <Input
-              {...register("firstName", {
-                required: "First Name Must Required",
-              })}
-              minW="100%"
-              m="0.5rem"
-              placeholder="Your irst Name"
-            />
-
-            <Field.Label>Last Name</Field.Label>
-            <Input
-              {...register("lastName", { required: "Last Name Must Required" })}
-              minW="100%"
-              m="0.5rem"
-              placeholder="Your Last Name"
-            />
-
-            <Field.Label>
-              Email<Field.RequiredIndicator></Field.RequiredIndicator>
-            </Field.Label>
-            <Field.HelperText>We'll never share your email.</Field.HelperText>
-            <Input
-              {...register("email", {
-                required: "Email Must Required",
-                pattern: {
-                  value: /^\S+@\S+$/i,
-                  message: "Invalid email",
-                },
-              })}
-              minW="100%"
-              m="0.5rem"
-              placeholder="Your Email"
-            />
-            <p>{errors.email?.message}</p>
-            <Field.Label>Phone</Field.Label>
-            <Input
-              minW="100%"
-              m="0.5rem"
-              {...register("phone", { required: "Phone Number Must Required" })}
-              type="number"
-              placeholder="Your Phone Number"
-            />
-
-            <Field.Label>
-              Password<Field.RequiredIndicator></Field.RequiredIndicator>{" "}
-            </Field.Label>
-            <Input
-              type="password"
-              {...register("password", {
-                required: "Password Field van't be empty ...",
-              })}
-              minW="100%"
-              m="0.5rem"
-              placeholder="Enter Strong Password"
-            />
-            <p>{errors.password?.message}</p>
-          </Field.Root>
-
-          <Button
-            bgColor={"wheat"}
-            color="#000"
-            minW="80px"
-            h="35px"
-            type="submit"
-          >
-            Submit
-          </Button>
-        </form>
-      </Center>
-    </Center>
+    <>
+      <Login />
+    </>
   );
 }
 
---
+++ Form_validations/src/Forms/Login.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { Center, Box, Text, Input, Field, Button } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  //   function handleLogin(data) {
+  //     console.log(data);
+  //   }
+  const handleLogin = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Center flexDirection={"column"} w="100%" h="100vh" bg="#ebebeb">
+      <Text textAlign={"center"} color="#000" fontSize="3xl">
+        Login
+      </Text>
+      <Box
+        p={{ base: "2", md: "4", lg: "6" }}
+        w={{ base: "95%", md: "40%", lg: "35%" }}
+        // minH="70vh"
+        borderRadius={"md"}
+        border={"0.5px solid #cbcbcbaa"}
+      >
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <Field.Root required>
+            <Field.Label color="#000">
+              Email <Field.RequiredIndicator></Field.RequiredIndicator>
+            </Field.Label>
+            <Input
+              {...register("email", {
+                required: "email must required",
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: "Invalid Email Address...",
+                },
+              })}
+              color={"#000"}
+              placeholder="Enter your mail"
+              type="text"
+              w="100%"
+            />
+            <Text fontSize="10px" color={"#eba626"}>
+              {" "}
+              {errors.email?.message}
+            </Text>
+            <Field.Label color="#000">
+              Password <Field.RequiredIndicator></Field.RequiredIndicator>
+            </Field.Label>
+            <Input
+              {...register("password", { required: "password must required" ,pattern:{
+                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/,
+                message:"Password must inclue 1 lower case 1 upper case and atleast length of 6-8 characters..."
+              }})}
+              placeholder="Enter your password"
+              color={"#000"}
+              type="password"
+              w="100%"
+            />
+            <Text fontSize="10px" color={"#eba626"}>
+              {errors.password?.message}
+            </Text>
+          </Field.Root>
+
+          <Button
+            type="submit"
+            mt="20px"
+            mb="20px"
+            w="100%"
+            bgColor="#38a0ea"
+            color="#fff"
+          >
+            Submit
+          </Button>
+        </form>
+      </Box>
+    </Center>
+  );
+}
+
+export default Login;
